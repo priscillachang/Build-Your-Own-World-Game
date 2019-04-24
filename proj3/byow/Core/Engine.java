07f93fe68c6ee8631438e7d6c7099325550f562b
Qinhong Chen <qinhong.chen@berkeley.edu>
2019-04-23 19:41:14 -0700

    removed TODO comment

--- proj3/byow/Core/Engine.java
+++ proj3/byow/Core/Engine.java
@@ -83,13 +83,6 @@ public class Engine {
      * @return the 2D TETile[][] representing the state of the world
      */
     public TETile[][] interactWithInputString(String input) {
-        // TODO: Fill out this method so that it run the engine using the input
-        // passed in as an argument, and return a 2D tile representation of the
-        // world that would have been drawn if the same inputs had been given
-        // to interactWithKeyboard().
-        //
-        // See proj3.byow.InputDemo for a demo of how you can make a nice clean interface
-        // that works for many different input types.
         input = input.toLowerCase();
         int begin = input.indexOf("n") + 1;
         int end = input.indexOf("s");
